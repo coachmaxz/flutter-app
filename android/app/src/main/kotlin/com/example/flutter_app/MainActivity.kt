a42daba7baec0271179a78f75b5cdbd0a4ba9258
Max Eakkabin Jaikeawma <eakkabin.max@outlook.com>
2024-09-15 09:00:15 +0700

    update android

--- android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_app/MainActivity.kt
@@ -1,5 +1,3 @@
 package com.example.flutter_app
-
 import io.flutter.embedding.android.FlutterActivity
-
 class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: Colors.blue,
